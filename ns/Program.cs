ed33f32e36d44442b24ad5b3e4a17f936dab6aa7
Eugenio Miró <eugenio.miro@gmail.com>
2016-06-12 08:34:29 -0300

    update ns test program to use TcpTable2

--- ns/Program.cs
+++ ns/Program.cs
@@ -8,13 +8,15 @@ namespace ns
     {
         static void Main(string[] args)
         {
-            var at = new TcpTable().ToArray();
+            var at = new TcpTable2().ToArray();
             Console.WriteLine($"Number of Entries: {at.Length}");
-            const String format = "{0:5} {1:-22} {2:-16} {3:10} {4:-16} {5:10}";
-            Console.WriteLine(String.Format(format, "TCP #", "State", "Local Addr", "Local Port", "Remote Addr", "Remote Port"));
+            const String format = "{0}\t{1,-12}\t{2,-15}\t{3,10}\t{4,-15}\t{5,11}\t{6,10}\t{7}";
+            Console.WriteLine(String.Format(format, 
+                    "TCP #", "State", "Local Addr", "Local Port", "Remote Addr", "Remote Port", "Owning PID", "Offload State"));
             var cnt = 0;
             foreach (var row in at) {
-                Console.WriteLine(String.Format(format, cnt, row.GetStateString(), row.LocalAddress, row.LocalPort, row.RemoteAddress, row.RemotePort));
+                Console.WriteLine(String.Format(format, 
+                    cnt++, row.GetStateString(), row.LocalAddress, row.LocalPort, row.RemoteAddress, row.RemotePort, row.OwningPid, row.GetOffloadStateString()));
             }
         }
     }
