a173f53c18f24656923f9a0adb23fe53f9912088
Eugenio Miró <eugenio.miro@gmail.com>
2016-08-03 10:35:30 -0300

    add buildNuget powershell script

---
+++ nuget/buildNuget.ps1
@@ -0,0 +1,22 @@
+param (
+    [string] $releaseNotes
+    )
+
+if (!(Test-path lib))      { mkdir lib }
+if (!(Test-path lib\4.0 )) { mkdir lib\4.0 }
+
+$libPath        = '..\x64\Release\Eunoia.Net.Collections.dll'
+$nugetSpecPath  = 'lib\4.0\Eunoia.Net.Collections.dll'
+$nuspecPath     = '.\Eunoia.Net.Collections.dll.nuspec'
+$libVersion     = Get-Item $libPath | % {[Reflection.AssemblyName]::GetAssemblyName($_.FullName).Version.ToString() }
+$libVersion
+Copy-Item $libPath lib\4.0 
+$xml            = [xml] (Get-Content $nuspecPath)
+
+$xml.package.metadata.version = $libVersion.ToString()
+if (![String]::IsNullOrWhiteSpace($releaseNotes)) { 
+    $xml.package.metadata.releaseNotes = $releaseNotes 
+}
+Set-Content $nuspecPath -Value $xml.OuterXml
+
+nuget pack $nuspecPath -Exclude *.ps1 -Exclude *.pfx
